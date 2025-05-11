bcd3109a87b8b8d1ad180cac7640466ad1b2353d
devlam <havulam1998z@gmail.com>
2025-05-11 17:41:31 +0700

    update: config defaul

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,23 +3,18 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@pinia/nuxt',
-  ],
-  css: ['~/assets/css/tailwind.css'],
-  runtimeConfig:{
+  modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+  css: ["~/assets/css/tailwind.css"],
+  runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL || 'http://localhost:8000', // Cấu hình API base URL
+      apiBase: process.env.API_BASE_URL || "http://localhost:8000", // Cấu hình API base URL
     },
   },
   components: [
     {
-      path: '~/components',
+      path: "~/components",
       pathPrefix: false,
     },
   ],
-  plugins: [
-    '~/plugins/axios.ts'
-  ],
-})
+  plugins: ["~/plugins/axios.ts"],
+});
